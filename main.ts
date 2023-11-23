d8258a5672db20267fe2e9a1397814e78a37043c
fcasadei3 <151800819+fcasadei3@users.noreply.github.com>
2023-11-22 20:10:46 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    sprite.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 3)
+basic.forever(function () {
+	
+})
